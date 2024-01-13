36edb419c398294b08a926e74db2b61a1e908339
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-13 11:14:35 -0800

    add Entry model

---
+++ DoJourAPI/Models/Entry.cs
@@ -0,0 +1,11 @@
+namespace DoJourAPI.Models 
+{
+  public class Entry
+  {
+    public int EntryId { get; set; }
+    public string Title { get; set; }
+    public string Subject { get; set; }
+    public string Date { get; set; }
+    public string Text { get; set; }
+  }
+}
