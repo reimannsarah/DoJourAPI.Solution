27b74459924f45c509425585dc167dd33d40b748
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-16 12:26:13 -0800

    passing test to get all entries

---
+++ DoJourAPI.Tests/Services/EntryServiceTests.cs
@@ -0,0 +1,35 @@
+using Moq;
+using DoJourAPI.Models;
+using DoJourAPI.Repositories;
+using DoJourAPI.Services;
+using Xunit;
+
+public class EntryServiceTests
+{
+  private Mock<IEntryRepository> _entryRepositoryMock;
+  private EntryService _entryService;
+
+  public EntryServiceTests()
+  {
+    _entryRepositoryMock = new Mock<IEntryRepository>();
+    _entryService = new EntryService(_entryRepositoryMock.Object);
+  }
+
+  [Fact]
+  public async Task GetByAllEntriesAsync_ShouldReturnAllEntries()
+  {
+    var expectedEntries = new[]
+    {
+      new Entry { EntryId = 1, Title = "Entry 1" },
+      new Entry { EntryId = 2, Title = "Entry 2" },
+      new Entry { EntryId = 3, Title = "Entry 3" }
+    };
+    _entryRepositoryMock
+      .Setup(x => x.GetAllAsync())
+      .ReturnsAsync(expectedEntries);
+
+    var actualEntries = await _entryService.GetAllEntriesAsync();
+
+    Assert.Equal(expectedEntries, actualEntries);
+  }
+}
