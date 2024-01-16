f19f1d4e465bb5b4de46a2d4456f7b45a28b95cc
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-16 13:59:17 -0800

    remove unnecessary using directives

--- DoJourAPI/Controllers/EntryController.cs
+++ DoJourAPI/Controllers/EntryController.cs
@@ -1,7 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using DoJourAPI.Repositories;
 using DoJourAPI.Services;
 using DoJourAPI.Models;
 
