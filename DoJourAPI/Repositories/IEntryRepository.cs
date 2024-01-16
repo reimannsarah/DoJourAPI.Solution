b0a611b9e69f28eeac0e6c70bb8e48168c0b0b5c
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-15 18:13:42 -0800

    add entry repository interface

---
+++ DoJourAPI/Repositories/IEntryRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoJourAPI.Models;
+using DoJourAPI.Repositories;
+
+namespace DoJourAPI.Repositories
+{
+    public interface IEntryRepository
+    {
+        // Task<Entry> GetByIdAsync(int id);
+        Task<IEnumerable<Entry>> GetAllAsync();
+        // Task CreateAsync(Entry entry);
+        // Task UpdateAsync(Entry entry);
+        // Task DeleteAsync(int id);
+    }
+}
