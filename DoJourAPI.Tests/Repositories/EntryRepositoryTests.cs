d715525a6bfab2a48206642ad88838f531350330
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-16 11:01:31 -0800

    passing test for post method

--- DoJourAPI.Tests/Repositories/EntryRepositoryTests.cs
+++ DoJourAPI.Tests/Repositories/EntryRepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using DoJourAPI.Repositories;
 using System.Collections.Generic;
 using Xunit;
+using DoJourAPI.Models;
 
 namespace DoJourAPI.Tests.Repositories
 {
@@ -31,6 +32,29 @@ namespace DoJourAPI.Tests.Repositories
             var result = await entryRepository.GetByIdAsync(1);
 
             Assert.Equal(1, result.EntryId);
+        }
+
+        [Fact]
+        public async Task CreateAsync_addsEntryToDatabase()
+        {
+            var helpers = new RepositoryTestHelpers();
+            var dbContextMock = helpers.GetDbContext(helpers.GetInitialEntities());
+            var entryRepository = helpers.EntryRepositoryInit(dbContextMock);
+
+            var newEntry = new Entry
+            {
+                EntryId = 4,
+                Title = "Test Title 4",
+                Subject = "Test Subject 4",
+                Date = "Test Date 4",
+                Text = "Test Text 4"
+            };
+
+            await entryRepository.CreateAsync(newEntry);
+
+            var result = await entryRepository.GetByIdAsync(4);
+
+            Assert.Equal(4, result.EntryId);
         }
     }
 }
