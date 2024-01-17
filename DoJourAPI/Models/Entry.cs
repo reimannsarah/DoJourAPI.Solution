1aa3294f14e4dffed48b2d6a23512ce0362840ec
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 10:47:08 -0800

    add foreign key and va property for user

--- DoJourAPI/Models/Entry.cs
+++ DoJourAPI/Models/Entry.cs
@@ -3,6 +3,8 @@ namespace DoJourAPI.Models
   public class Entry
   {
     public int EntryId { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
     public string Title { get; set; }
     public string Subject { get; set; }
     public string Date { get; set; }
