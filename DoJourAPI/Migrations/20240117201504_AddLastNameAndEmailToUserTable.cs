4e95b7fb5c9ce29d0deb1157a87c7a797a7a78c4
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 12:15:36 -0800

    add last name and email to user model/table

---
+++ DoJourAPI/Migrations/20240117201504_AddLastNameAndEmailToUserTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoJourAPI.Migrations
+{
+    public partial class AddLastNameAndEmailToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+        }
+    }
+}
--- DoJourAPI/Models/User.cs
+++ DoJourAPI/Models/User.cs
@@ -4,6 +4,8 @@ namespace DoJourAPI.Models
   {
     public int UserId { get; set; }
     public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email  { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public ICollection<Entry> Entries { get; set; }
