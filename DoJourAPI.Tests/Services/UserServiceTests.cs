be5771ff61b03adeda8770445a282901392d45da
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 14:40:30 -0800

    passing test for getuserbyId

---
+++ DoJourAPI.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,32 @@
+using Moq;
+using DoJourAPI.Models;
+using DoJourAPI.Repositories;
+using DoJourAPI.Services;
+using Xunit;
+
+namespace DoJourAPI.Tests.Services;
+
+public class UserServiceTests
+{
+  private readonly Mock<IUserRepository> _mockUserRepository;
+  private readonly UserService _userService;
+
+  public UserServiceTests()
+  {
+    _mockUserRepository = new Mock<IUserRepository>();
+    _userService = new UserService(_mockUserRepository.Object);
+  }
+
+  [Fact]
+  public async Task GetUserByIdAsync_ShouldReturnUserWithMatchingId()
+  {
+    var expectedUser = new User { UserId = Guid.NewGuid(), FirstName = "User 1" };
+    _mockUserRepository
+      .Setup(x => x.GetByIdAsync(expectedUser.UserId))
+      .ReturnsAsync(expectedUser);
+
+    var actualUser = await _userService.GetUserByIdAsync(expectedUser.UserId);
+
+    Assert.Equal(expectedUser, actualUser);
+  }
+}
