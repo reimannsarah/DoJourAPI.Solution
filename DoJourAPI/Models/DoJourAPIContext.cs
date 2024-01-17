b85305f63c2619f31173667b8e793b5f55ac8751
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 10:49:24 -0800

    update db context

--- DoJourAPI/Models/DoJourAPIContext.cs
+++ DoJourAPI/Models/DoJourAPIContext.cs
@@ -8,5 +8,13 @@ namespace DoJourAPI.Models
     public virtual DbSet<Entry> Entries { get; set; }
     public virtual DbSet<User> Users { get; set; }
     public DoJourAPIContext(DbContextOptions<DoJourAPIContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entry>()
+            .HasOne(e => e.User)
+            .WithMany(u => u.Entries)
+            .HasForeignKey(e => e.UserId);
+    }
   }
 }
