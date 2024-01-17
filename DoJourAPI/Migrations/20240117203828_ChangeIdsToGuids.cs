23f52f61c1a275a56d253ef48de89a7b3c1ef3c2
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 12:42:59 -0800

    change ids to Guids

---
+++ DoJourAPI/Migrations/20240117203828_ChangeIdsToGuids.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoJourAPI.Migrations
+{
+    public partial class ChangeIdsToGuids : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Users_UserId",
+                table: "Entries");
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Users",
+                type: "char(36)",
+                nullable: false,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Entries",
+                type: "char(36)",
+                nullable: false,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Users_UserId",
+                table: "Entries",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Users_UserId",
+                table: "Entries");
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Entries",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Users_UserId",
+                table: "Entries",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
