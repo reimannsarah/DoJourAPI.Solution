fc8962fae95a3e36164c3cd6551d85da2d69a5ee
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-17 10:47:14 -0800

    add user model

---
+++ DoJourAPI/Models/User.cs
@@ -0,0 +1,11 @@
+namespace DoJourAPI.Models 
+{
+  public class User
+  {
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public ICollection<Entry> Entries { get; set; }
+  }
+}
