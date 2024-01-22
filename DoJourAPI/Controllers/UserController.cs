15d1a60a103e944a0ba329ceaa06737f2b18f6d9
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 10:36:15 -0800

    add user controller with login and register methods

---
+++ DoJourAPI/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using DoJourAPI.Services;
+using DoJourAPI.Models;
+
+namespace DoJourAPI.Controllers;
+
+[ApiController]
+[Route("api/users")]
+
+public class UsersController : ControllerBase
+{
+  private readonly IUserService _userService;
+  public UsersController(IUserService userService)
+  {
+    _userService = userService;
+  }
+
+  [HttpPost("register")]
+  public async Task<IActionResult> RegisterUser(User user)
+  {
+    var existingUser = await _userService.GetUserByEmailAsync(user.Email);
+    if (existingUser != null)
+    {
+      return BadRequest("A user with this email already exists.");
+    }
+
+    user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+    await _userService.CreateUserAsync(user);
+    return Ok();
+  }
+
+  [HttpPost("login")]
+  public async Task<IActionResult> LoginUser(User user)
+  {
+    var foundUser = await _userService.GetUserByEmailAsync(user.Email);
+    if (foundUser == null)
+    {
+      return NotFound();
+    }
+    if (!BCrypt.Net.BCrypt.Verify(user.Password, foundUser.Password))
+    {
+      return Unauthorized();
+    }
+    return Ok(foundUser);
+  }
+}
