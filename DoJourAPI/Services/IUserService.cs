10ff5cfe2604d228da5523c42ceb57881cf6ace7
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 10:36:45 -0800

    add method to get user by email

--- DoJourAPI/Services/IUserService.cs
+++ DoJourAPI/Services/IUserService.cs
@@ -5,6 +5,7 @@ namespace DoJourAPI.Services;
 public interface IUserService
 {
     Task<User> GetUserByIdAsync(Guid id);
+    Task<User> GetUserByEmailAsync(string email);
     Task CreateUserAsync(User user);
     Task UpdateUserAsync(User user);
     Task DeleteUserAsync(Guid id);
