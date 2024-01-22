6a59f3b47889a5a40b68324f76d96cb165a33346
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 12:56:39 -0800

    configure authentication

--- DoJourAPI/Program.cs
+++ DoJourAPI/Program.cs
@@ -2,10 +2,11 @@ using DoJourAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using DoJourAPI.Services;
 using DoJourAPI.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.dot
 
 builder.Services.AddControllers();
 
@@ -18,6 +19,21 @@ builder.Services.AddDbContext<DoJourAPIContext>(
                         )
                         )
                     );
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });                  
 
 builder.Services.AddCors(options => 
 {
@@ -38,7 +54,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
