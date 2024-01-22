382bcc093b4a2e46db5f2440f9d9e716268baf73
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 13:10:05 -0800

    add data annotation

--- DoJourAPI/Models/User.cs
+++ DoJourAPI/Models/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DoJourAPI.Models 
 {
   public class User
@@ -5,8 +7,12 @@ namespace DoJourAPI.Models
     public Guid UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
     public string Email  { get; set; }
     public string Username { get; set; }
+    [Required]
+    [MinLength(8)]
     public string Password { get; set; }
     public ICollection<Entry> Entries { get; set; }
   }
