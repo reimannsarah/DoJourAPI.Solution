77007d9184160b2950a7cf60efff2894e0b2555a
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-22 10:36:31 -0800

    add method to retrieve user by email

--- DoJourAPI/Repositories/IUserRepository.cs
+++ DoJourAPI/Repositories/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace DoJourAPI.Repositories
     public interface IUserRepository
     {
         Task<User> GetByIdAsync(Guid id);
+        Task<User> GetByEmailAsync(string email);
         Task CreateAsync(User user);
         Task UpdateAsync(User user);
         Task DeleteAsync(Guid id);
