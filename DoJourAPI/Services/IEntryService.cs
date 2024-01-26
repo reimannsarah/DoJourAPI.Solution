5cf1b4b0a5231cb499fa56890a6b3215da2b72f1
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-26 15:18:38 -0800

    Get EntriesByUserID

--- DoJourAPI/Services/IEntryService.cs
+++ DoJourAPI/Services/IEntryService.cs
@@ -6,6 +6,7 @@ public interface IEntryService
 {
     Task<Entry> GetEntryByIdAsync(Guid id);
     Task<IEnumerable<Entry>> GetAllEntriesAsync();
+    Task<IEnumerable<Entry>> GetEntriesByUserIdAsync(Guid userId);
     Task CreateEntryAsync(Entry entry);
     Task UpdateEntryAsync(Entry entry);
     Task DeleteEntryAsync(Guid id);
